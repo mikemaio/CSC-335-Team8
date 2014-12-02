07e86793b85337605d25865a847b983a4626baca
kdubsky <kdubsky@callutheran.edu>
2014-12-02 12:05:01 -0800

    Doesn't create account with bad fields

--- src/ClientServer/RegisterPanel.java
+++ src/ClientServer/RegisterPanel.java
@@ -70,113 +70,30 @@ public class RegisterPanel extends JPanel{
 						String account = accountRegP.getText();
 						String password = passwordRegP.getText();
 						String email = emailRegP.getText(); 
-						gr.clientOne.sendString("registerprocedure",account,password,email);
-						String delims = "[,]";
-						String recievedData[] = gr.clientOne.getflowValues().split(delims);
-						boolean correctFormat = true;
-						boolean passNum = false;
-						boolean passLower = false;
-						boolean passUpper = false;
-
 						
-						if (accountRegP.getText().length() < USERMIN) 
+						
+						if (correctFields()) 
 						{
-							JOptionPane.showMessageDialog(createRegP, "Username must be at least 5 characters long.");
-							accountRegP.setText("");
-							correctFormat = false;
-						}
-						
-						for(int i = 0; i < password.length(); i++) {
-							if (Character.isDigit(password.charAt(i))) 
-							{
-								passNum = true;
-							}
-							if (Character.isLowerCase(password.charAt(i))) 
-							{
-								passLower = true;
-							}
-							if (Character.isUpperCase(password.charAt(i))) 
-							{
-								passUpper = true;
-							}
-						}
-						
-						if (!(passNum && passLower && passUpper)) {
-							JOptionPane.showMessageDialog(createRegP, "Password must have at least one lowercase letter, one uppercase letter, and one number in it.");
-							correctFormat = false;
-						}
-						
-						if (passwordRegP.getText().length() < USERMIN) 
-						{
-							JOptionPane.showMessageDialog(createRegP, "Password must be at least 5 characters long.");
-							passwordRegP.setText("");
-							confirmPassRegP.setText("");
-							correctFormat = false;
-						}
-						
-						if (!passwordRegP.getText().equals(confirmPassRegP.getText())) 
-						{
-							JOptionPane.showMessageDialog(createRegP, "Passwords do not match.");
-							confirmPassRegP.setText("");
-							correctFormat = false;
-						}
-						
-						if (!validEmailAddress(email)) 
-						{
-							JOptionPane.showMessageDialog(createRegP, "Invalid email address.");
-							emailRegP.setText("");
-							emailConfirmRegP.setText("");
-							correctFormat = false;
-						}
-						
-						if(!emailRegP.getText().equals(emailConfirmRegP.getText()))
-						{
-							JOptionPane.showMessageDialog(createRegP, "Emails do not match.");
-							emailConfirmRegP.setText("");
-							correctFormat = false;
-						}
-						
-						if (recievedData[0].equals("exists"))
-						{
-							JOptionPane.showMessageDialog(createRegP, "Desired username already exists.");
-							accountRegP.setText("");
-							correctFormat = false;
-						}
-						
-						if (correctFormat) 
-						{
-							clearFields();
-							JOptionPane.showMessageDialog(createRegP, "Account Created!!!!");
-							Client.sendEmail(email, 
-									"You have registered!\n\nYour username is: " 
-									+ account + "\n\n Your password is: " + password);
-							gr.refreshGUI(2);
-						}
-							
-						/*if (correctFormat) {
 							gr.clientOne.sendString("registerprocedure",account,password,email);
 							String delims = "[,]";
 							String recievedData[] = gr.clientOne.getflowValues().split(delims);
-							if(recievedData[0].equals("created"))
+							JOptionPane.showMessageDialog(createRegP, "Correct fields.");
+
+							if (recievedData[0].equals("exists"))
 							{
+								JOptionPane.showMessageDialog(createRegP, "Desired username already exists.");
+								accountRegP.setText("");
+
+							} else if (recievedData[0].equals("created")) {
+							
 								clearFields();
 								JOptionPane.showMessageDialog(createRegP, "Account Created!!!!");
+								Client.sendEmail(email, 
+										"You have registered!\n\nYour username is: " 
+												+ account + "\n\n Your password is: " + password);
 								gr.refreshGUI(2);
 							}
-							else if(recievedData[0].equals("exists"))
-							{
-								clearFields();
-								JOptionPane.showMessageDialog(createRegP, "The account desired already exists please try again.");
-								confirmPassRegP.setText("");
-
-							}
-							else
-							{
-								System.out.println("Error with Register panel");
-							}
-						}*/
-						//clearFields();
-
+						}
 					}
 				}
 			);
@@ -204,6 +121,74 @@ public class RegisterPanel extends JPanel{
     {
         Matcher matcher = pattern.matcher(emailaddress);
         return matcher.find();
+    }
+    
+    public boolean correctFields() {
+    	boolean correctFormat = true;
+    	boolean passNum = false;
+		boolean passLower = false;
+		boolean passUpper = false;
+		   	
+    	if (accountRegP.getText().length() < USERMIN) 
+		{
+			JOptionPane.showMessageDialog(createRegP, "Username must be at least 5 characters long.");
+			accountRegP.setText("");
+			correctFormat = false;
+		}
+    	
+    	for(int i = 0; i < passwordRegP.getText().length(); i++) {
+			if (Character.isDigit(passwordRegP.getText().charAt(i))) 
+			{
+				passNum = true;
+			}
+			if (Character.isLowerCase(passwordRegP.getText().charAt(i))) 
+			{
+				passLower = true;
+			}
+			if (Character.isUpperCase(passwordRegP.getText().charAt(i))) 
+			{
+				passUpper = true;
+			}
+		}
+		
+		if (!(passNum && passLower && passUpper)) {
+			JOptionPane.showMessageDialog(createRegP, "Password must have at least one lowercase letter, one uppercase letter, and one number in it.");
+			correctFormat = false;
+		}
+		
+		if (passwordRegP.getText().length() < USERMIN) 
+		{
+			JOptionPane.showMessageDialog(createRegP, "Password must be at least 5 characters long.");
+			passwordRegP.setText("");
+			confirmPassRegP.setText("");
+			correctFormat = false;
+		}
+		
+		if (!passwordRegP.getText().equals(confirmPassRegP.getText())) 
+		{
+			JOptionPane.showMessageDialog(createRegP, "Passwords do not match.");
+			confirmPassRegP.setText("");
+			correctFormat = false;
+		}
+		
+		if(!emailRegP.getText().equals(emailConfirmRegP.getText()))
+		{
+			JOptionPane.showMessageDialog(createRegP, "Emails do not match.");
+			emailConfirmRegP.setText("");
+			correctFormat = false;
+		}
+		
+		if (!validEmailAddress(emailRegP.getText())) 
+		{
+			JOptionPane.showMessageDialog(createRegP, "Invalid email address.");
+			emailRegP.setText("");
+			emailConfirmRegP.setText("");
+			correctFormat = false;
+		}
+		
+		
+		
+    	return correctFormat;
     }
 	
 }
