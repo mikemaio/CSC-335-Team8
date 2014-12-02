a6bbcfa065fefa11eb769e6130149d06fe0e86e3
kdubsky <kdubsky@callutheran.edu>
2014-12-02 14:45:40 -0800

    Added method for sending emails.

--- src/ClientServer/Client.java
+++ src/ClientServer/Client.java
@@ -7,6 +7,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Properties;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class Client {
 
@@ -36,11 +41,43 @@ public class Client {
 	//    to be opened on top of the socket
 	private BufferedReader datain;
 	private DataOutputStream dataout;
+	private GUIRoot _guiRoot;
+	//loging values sent from connection thread
+	private String flowValues = "";
+	//user values
+	private int usersConnected;
+	
+	// -- set the gmail host URL
+	final static private String host = "smtp.gmail.com";
 
+	// -- You must have a valid gmail username/password pair to use
+	// gmail as a SMTP service
+	final static private String username = "CLUCSC335";
+	final static private String password = "CLUC$C335";
+	
+	//Username character min value
+	public static final int USERMIN = 4;
 	  	
-	public Client ()
+	public String getflowValues() {
+		return flowValues;
+	}
+
+	public void setUserQueryValue(int users)
 	{
-		
+		this.usersConnected = users;
+	}
+	public int getUserQueryValue()
+	{
+		return usersConnected;
+	}
+	public void setflowValues(String Values) {
+		this.flowValues = Values;
+	}
+
+
+	public Client (GUIRoot gr)
+	{
+		_guiRoot = gr;
 		try {
 			System.out.print("Connect:");
 			// -- construct the peer to peer socket
@@ -58,15 +95,15 @@ public class Client {
 		
 	}
 	
-	public String sendString (String _msg)
+	
+	public String sendString (String _msg, String account, String password, String email )
 	{
 		String rtnmsg = "";
-
 		try {
 			// -- the server only receives String objects that are
 			//    terminated with a newline \n"
 			// -- send the String making sure to flush the buffer
-			dataout.writeBytes(_msg + "\n");
+			dataout.writeBytes(_msg + "," + account+ "," + password + "," + email + "\n");
 			dataout.flush();
 			
 			// -- receive the response from the server
@@ -75,7 +112,51 @@ public class Client {
 			//    that to occur.
 			rtnmsg = "";
 			do {
+				
 				rtnmsg = datain.readLine();
+				//put into if statements for potential future use
+				String delims = "[,]";
+				String recievedData[] = rtnmsg.split(delims);
+				if(recievedData[0].equals("success") && recievedData[1].equals("account"))
+				{
+					flowValues = rtnmsg;
+				}
+				else if(recievedData[0].equals("invalid") && recievedData[1].equals("account"))
+				{
+					flowValues = rtnmsg;
+				}
+				else if(recievedData[0].equals("missing") && recievedData[1].equals("account"))
+				{
+					flowValues = rtnmsg;
+				}
+				else if(recievedData[0].equals("locked") && recievedData[1].equals("account"))
+				{
+					flowValues = rtnmsg;
+				}
+				else if(recievedData[0].equals("exists") && recievedData[1].equals("account"))
+				{
+					flowValues = rtnmsg;
+				}
+				else if(recievedData[0].equals("created") && recievedData[1].equals("account"))
+				{
+					flowValues = rtnmsg;
+				}
+				else if(recievedData[0].equals("failedpasschange") && recievedData[1].equals("account"))
+				{
+					flowValues = rtnmsg;
+				}
+				else if(recievedData[0].equals("oldpasswordfail") && recievedData[1].equals("account"))
+				{
+					flowValues = rtnmsg;
+				}	
+				else if(recievedData[0].equals("users"))
+				{
+					usersConnected = Integer.parseInt(recievedData[1]); 
+				}
+				else
+				{
+					System.out.println("Client Error");
+				}
 			} while (rtnmsg.equals(""));
 						
 		} catch (IOException e) {
@@ -109,17 +190,68 @@ public class Client {
 		
 	}
 	
+	public static void sendEmail(String to, String content) {
+		// -- set up host properties
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", host);
+		props.put("mail.smtp.port", "587");
+
+		// -- Get the Session object.
+		Session session = Session.getInstance(props,
+				new javax.mail.Authenticator() {
+					protected PasswordAuthentication getPasswordAuthentication() {
+						return new PasswordAuthentication(username, password);
+					}
+				});
+
+		// -- Set up the sender's email account information
+		String from = "CLUCSC335@gmail.com";
+				
+		try {
+			// -- Create a default MimeMessage object.
+			Message message = new MimeMessage(session);
+
+			// -- Set From: header field of the header.
+			message.setFrom(new InternetAddress(from));
+
+			// -- Set To: header field of the header.
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+
+			// -- Set Subject: header field
+			message.setSubject("CSC335 Project");
+
+			// Now set the actual message
+			message.setText(content);
+
+			// -- Send message
+			// -- use either these three lines or...
+			// Transport t = session.getTransport("smtp");
+			// t.connect();
+			// t.sendMessage(message, message.getAllRecipients());
+			
+			// -- .. this one (which ultimately calls sendMessage(...)
+			Transport.send(message);
+
+			System.out.println("Sent message successfully....");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
 	
 	public static void main(String[] args) {
 		// -- instantiate a Client object
 		//    the constructor will attempt to connect to the server
-		Client client = new Client();
+		//Client client = new Client();
 		
 		// -- send message to server and receive reply.
-		String commandString = "hello";
+	/*	String commandString = "hello";
 		System.out.println("CLIENT send:  " + commandString);
-		String replyString = client.sendString(commandString);
-		System.out.println("CLIENT receive: " + replyString);
+		//String replyString = client.sendString(commandString);
+		//System.out.println("CLIENT receive: " + replyString);
 		
 		for (int i = 0; i < 60; ++i) {
 			try {
@@ -134,7 +266,7 @@ public class Client {
 			System.out.println("CLIENT receive: " + replyString);
 		}
 
-		client.disconnect();
+		client.disconnect();**/
 	}
 
 }
